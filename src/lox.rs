56e0d6e3edc22b383742590816ce475d415e6078
Marcelo Morales <morales.374@osu.edu>
2025-10-31 23:09:33 -0400

    Adding boilerplates

---
+++ src/lox.rs
@@ -0,0 +1,7 @@
+pub fn run_file(file_name: &str) {
+    println!("Lox says: {file_name}");
+}
+
+pub fn run_prompt() {
+    unimplemented!("run_prompt is not yet implemented");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::process;
+
+mod lox;
 fn main() {
-    println!("Hello, world!");
+    let arguments: Vec<String> = env::args().collect();
+    let number_of_arguments: usize = arguments.len();
+
+    if number_of_arguments > 3 {
+        eprintln!("Usage: cargo run main [script]");
+        process::exit(64);
+    } else if number_of_arguments == 3 {
+        lox::run_file("hello there!");
+    } else {
+        lox::run_prompt();
+    }
 }
