f64e1e05ec941464806b630f81aa302e299afc1b
Marcelo Morales <morales.374@osu.edu>
2026-01-23 22:57:58 -0500

    lox.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
-use std::env;
 use std::process::ExitCode;
 
+mod lox;
+
 fn main() -> ExitCode {
-    let args: Vec<String> = env::args().collect();
-
-    let number_of_args: usize = args.len();
-
-    if number_of_args > 2 {
-        println!("Usage: cargo run [script]");
-        return ExitCode::from(64);
-    } else if number_of_args == 2 {
-        println!("running file {}", args[1])
-    } else {
-        println!("running prompt")
-    }
-
-    return ExitCode::SUCCESS;
+    return lox::main();
 }
