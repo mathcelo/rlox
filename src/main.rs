5fdf099a9eb5070f778ebcfd6c949207ca5e9efa
Marcelo Morales <morales.374@osu.edu>
2026-01-23 22:43:00 -0500

    Initial main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let number_of_args: usize = args.len();
+
+    if number_of_args > 2 {
+        println!("Usage: cargo run [script]")
+    } else if number_of_args == 2 {
+        println!("running file {}", args[1])
+    } else {
+        println!("running prompt")
+    }
 }
